b3fa27c1f29d34474f71fb769aa87f2fc0c1f0e8
lyu1 <leon.yu@illumina.com>
2023-09-21 21:43:59 +0800

    first commit from template

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER "drf-play" SUPERUSER;
+CREATE DATABASE "drf-play" OWNER "drf-play" ENCODING 'utf-8';
